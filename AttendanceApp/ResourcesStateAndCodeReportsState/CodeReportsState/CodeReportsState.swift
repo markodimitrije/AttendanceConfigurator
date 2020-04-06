7935978e664fd69c5c542fda1785d81c79b5e7c1
Dimitrijevic Marko <markodimitrije@gmail.com>
2020-04-06 16:55:31 +0200

    cleaner code reporting to web in CodeReportsState(Worker)

--- AttendanceApp/ResourcesStateAndCodeReportsState/CodeReportsState/CodeReportsState.swift
+++ AttendanceApp/ResourcesStateAndCodeReportsState/CodeReportsState/CodeReportsState.swift
@@ -35,41 +35,32 @@ class CodeReportsState { // ovo je trebalo da zoves viewModel-om !
     
     private func bindInputWithOutput() { print("CodeReportsState.bindInputWithOutput")
         
-        codeReport
-            .asObservable()
-            .subscribe(onNext: { [weak self] report in
+        codeReport.skip(1) // init with nil
+        .asObservable()
+        .subscribe(onNext: { [weak self] report in
+            
+            guard let sSelf = self else {return}
+            sSelf.repository.saveToRealm(codeReport: report!)
+            .subscribe { (success) in
+                sSelf.apiController.reportSingleCode(report: report!)
                 
-                guard let sSelf = self else {return}
-                let obs = sSelf.reportImidiatelly(codeReport: sSelf.codeReport.value)
-                obs
-                    .subscribe(onNext: { (code, success) in
-                        
-                        sSelf.webNotified.accept((code, success)) // postavi na svoj Output
-                        
+                    .subscribe(onNext: { (report, success) in
                         if success {
-                            sSelf.repository.update(codesAcceptedFromWeb: [code])
-                                .subscribe(onNext: { saved in
-                                    print("code successfully reported to web, save in your archive")
-                                }).disposed(by: sSelf.bag)
+                            sSelf.repository.update(codesAcceptedFromWeb: [report])
+                        } else {
+                            sSelf.codeReportFailed(report)
                         }
-                        
-                        if !success {
-                            sSelf.codeReportFailed(code) // izmestac code
-                        }
-                        
                     })
                     .disposed(by: sSelf.bag)
-            })
-            .disposed(by: bag)
+            }.disposed(by: sSelf.bag)
+        })
+        .disposed(by: bag)
         
     }
     
     private func codeReportFailed(_ report: ICodeReport) {
         
-        print("codeReportFailed/ snimi ovaj report.code \(report.getCode()) u realm")
-        
-        _ = repository.saveToRealm(codeReport: report)
-        // okini process da javljas web-u sve sto ima u realm (codes)
+        print("codeReportFailed, pokreni proces da javljas ovaj report.....")
         if codesDumper == nil {
             codesDumper = CodesDumperFactory.make() // u svom init, zna da javlja reports web-u...
             codesDumper.oCodesDumped
@@ -81,14 +72,5 @@ class CodeReportsState { // ovo je trebalo da zoves viewModel-om !
                 })
                 .disposed(by: bag)
         }
-        
     }
-    
-    private func reportImidiatelly(codeReport: ICodeReport?) -> Observable<(ICodeReport, Bool)> {
-        
-        guard let report = codeReport else {return Observable.empty()}
-        let codeReportApi = CodeReportApiControllerFactory.make()
-        return codeReportApi.reportSingleCode(report: report)
-    }
-    
 }
