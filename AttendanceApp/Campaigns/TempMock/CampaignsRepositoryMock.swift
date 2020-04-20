8e55e8616db0ef5f625379018f6ec13e2c987029
Dimitrijevic Marko <markodimitrije@gmail.com>
2020-04-20 19:20:33 +0200

    mock observable made from Observable.interval, changes emitted values every 2 sec

--- AttendanceApp/Campaigns/TempMock/CampaignsRepositoryMock.swift
+++ AttendanceApp/Campaigns/TempMock/CampaignsRepositoryMock.swift
@@ -10,10 +10,23 @@ import RxSwift
 
 class CampaignsRepositoryMock: ICampaignsImmutableRepository {
     func getAll() -> Observable<[ICampaign]> {
-        return Observable.create { (observer) -> Disposable in
-            observer.onNext(campaigns1)
-            observer.onCompleted()
-            return Disposables.create()
+        return Observable<Int>.interval(RxTimeInterval(2), scheduler: MainScheduler.instance).map(CampaignsFactory.getCampaigns)
+//        return Observable.create { (observer) -> Disposable in
+//            observer.onNext(campaigns1)
+//            observer.onCompleted()
+//            return Disposables.create()
+//        }
+    }
+}
+
+class CampaignsFactory {
+    static func getCampaigns(value: Int) -> [ICampaign] {
+        switch value {
+        case value where (value % 3) == 0: return campaigns1
+        case value where (value % 3) == 1: return campaigns2
+        case value where (value % 3) == 2: return campaigns3
+        default:
+            fatalError()
         }
     }
 }
