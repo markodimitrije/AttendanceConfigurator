f9b513d217491b8bb13c0d26d268c1907e4556b9
Dimitrijevic Marko <markodimitrije@gmail.com>
2020-04-29 13:26:34 +0200

    CampaignResourcesWorker.swift has func for testing err case (later remove)

--- AttendanceApp/ResourcesStateAndCodeReportsState/ResourcesState/CampaignResourcesProvider/CampaignResourcesWorker/CampaignResourcesWorker.swift
+++ AttendanceApp/ResourcesStateAndCodeReportsState/ResourcesState/CampaignResourcesProvider/CampaignResourcesWorker/CampaignResourcesWorker.swift
@@ -10,16 +10,28 @@ import RxSwift
 
 extension CampaignResourcesWorker: ICampaignResourcesWorker {
     func work() -> Observable<Void> {
-        resourcesApiController.fetch().take(1)
-            .do(onNext: { (resources) in
-                print("CampaignResourcesWorker.work PERSIST resources")
-                self.roomsRepo.save(rooms: resources.getLocations())
-                self.blocksRepo.save(blocks: resources.getSessions())
-                _ = self.delegatesRepo.save(delegates: resources.getDelegates())
-            }).map {_ in return ()}
-            .subscribeOn(MainScheduler.instance)
-            
+//        fetchCampaignResourcesAndSaveToRealm()
+        fetchCampaignResourcesGotError() //test
     }
+    
+    private func fetchCampaignResourcesAndSaveToRealm() -> Observable<Void> {
+        resourcesApiController.fetch().take(1)
+        .do(onNext: { (resources) in
+            print("CampaignResourcesWorker.work PERSIST resources")
+            self.roomsRepo.save(rooms: resources.getLocations())
+            self.blocksRepo.save(blocks: resources.getSessions())
+            _ = self.delegatesRepo.save(delegates: resources.getDelegates())
+        }).map {_ in return ()}
+        .subscribeOn(MainScheduler.instance)
+    }
+    
+    private func fetchCampaignResourcesGotError() -> Observable<Void> { // test
+        return Observable.create { (observer) -> Disposable in
+            observer.onError(LoginValidationError.emailNotValid)
+            return Disposables.create()
+        }.delaySubscription(1, scheduler: MainScheduler.instance)
+    }
+    
 }
 
 struct CampaignResourcesWorker {
