21128a0246c473df2fa4267476a6c6eaa42c0acc
Dimitrijevic Marko <markodimitrije@gmail.com>
2020-05-12 15:46:28 +0200

    move protocols into their own files

---
+++ AttendanceApp/Model/Persisters/CodeReportsRepositories/ICodeReportsImmutableRepository.swift
@@ -0,0 +1,9 @@
+//
+//  ICodeReportsImmutableRepository.swift
+//  AttendanceApp
+//
+//  Created by Marko Dimitrijevic on 12/05/2020.
+//  Copyright © 2020 Navus. All rights reserved.
+//
+
+import Foundation
