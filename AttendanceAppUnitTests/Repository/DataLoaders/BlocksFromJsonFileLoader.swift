4b324f9f9519ae1c02cf625b8ca62d7384be93e8
Dimitrijevic Marko <markodimitrije@gmail.com>
2020-05-15 17:55:42 +0200

    dateJsonDecoder needed for tests (blocks.json contains string dates while object need real Date)

--- AttendanceAppUnitTests/Repository/DataLoaders/BlocksFromJsonFileLoader.swift
+++ AttendanceAppUnitTests/Repository/DataLoaders/BlocksFromJsonFileLoader.swift
@@ -13,8 +13,17 @@ class BlocksFromJsonFileLoader {
     static func make(filename: String) -> [IBlock] {
         let bundle = Bundle(for: self)
         let jsonData = JsonBundleDataProvider().jsonData(filename: filename, inBundle: bundle)
-        let decoder = BlockDecoderFactory.make()//JSONDecoder()
+        let decoder = DateDecoderFactory.make()
         let blocksObject = try! decoder.decode(Blocks.self, from: jsonData)
         return blocksObject.data
+    }
+}
+
+class DateDecoderFactory {
+    static func make() -> JSONDecoder {
+        let dateFormatter = DateFormatter.init(format: Date.defaultFormatString)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = JSONDecoder.DateDecodingStrategy.formatted(dateFormatter)
+        return decoder
     }
 }
